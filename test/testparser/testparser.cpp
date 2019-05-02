9cc99aae5a76c99c8008c09af9a547da921517a3
Samuel Albrecht <elsamuko@gmail.com>
2019-05-02 15:45:26 +0200

    fix: server hello extension parsing w/out values

--- test/testparser/testparser.cpp
+++ test/testparser/testparser.cpp
@@ -4,7 +4,8 @@
 #include "clienthelloparser.hpp"
 #include "serverhelloparser.hpp"
 #include "client_hello_raw.hpp"
-#include "server_hello_raw.hpp"
+#include "server_hello_news.ycombinator.com.hpp"
+#include "server_hello_heise.de.hpp"
 
 BOOST_AUTO_TEST_CASE( client_parse_good ) {
     std::string data( reinterpret_cast<const char*>( client_hello_raw ), sizeof( client_hello_raw ) );
@@ -20,12 +21,17 @@ BOOST_AUTO_TEST_CASE( client_parse_bad ) {
     BOOST_CHECK( !parser.parse() );
 }
 
-BOOST_AUTO_TEST_CASE( server_parse_good ) {
-    std::string data( reinterpret_cast<const char*>( server_hello_raw ), sizeof( server_hello_raw ) );
+void testParseGood( const unsigned char cdata[], const size_t size, const uint64_t tlsSize ) {
+    std::string data( reinterpret_cast<const char*>( cdata ), size );
     ServerHelloParser parser( data );
     BOOST_REQUIRE( parser.parse() );
     parser.dump();
-    BOOST_CHECK_EQUAL( parser.hello()->size(), 57 );
+    BOOST_CHECK_EQUAL( parser.hello()->size(), tlsSize );
+}
+
+BOOST_AUTO_TEST_CASE( server_parse_good ) {
+    testParseGood( server_hello_news_ycombinator_com_raw, sizeof( server_hello_news_ycombinator_com_raw ), 57 );
+    testParseGood( server_hello_heise_de_raw, sizeof( server_hello_heise_de_raw ), 61 );
 }
 
 BOOST_AUTO_TEST_CASE( server_parse_bad ) {
