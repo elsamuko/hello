5e4070e37301b85a9f8e2fdebbd62eeaa1f2e562
Samuel Albrecht <elsamuko@gmail.com>
2019-05-08 22:47:31 +0200

    chg: build with boost date_time

--- scripts/build_boost.sh
+++ scripts/build_boost.sh
@@ -17,7 +17,7 @@ esac
 
 PROJECT=boost
 # ./b2 --show-libraries
-NEEDED_LIBS="--with-system --with-test"
+NEEDED_LIBS="--with-system --with-test --with-date_time"
 VERSION="1.70.0"
 VERSION_DL="${VERSION//./_}"
 DL_URL="https://dl.bintray.com/boostorg/release/${VERSION}/source/boost_${VERSION_DL}.tar.gz" 
